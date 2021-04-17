1f3e862acaebb2c0967fdf420bae60a3a4d809a0
Claudine LAFINE <claudine.lafine@gmail.com>
2021-04-17 19:57:40 +0200

    FruitService

---
+++ tests/unit/FruitService.spec.js
@@ -0,0 +1,193 @@
+import data from './data'
+import retrieveFruitsService from '@/services/FruitService'
+
+let fruitService
+let apiClient
+
+beforeEach(() => {
+  apiClient = {
+    get: jest.fn(),
+    delete: jest.fn(),
+    post: jest.fn(),
+  }
+  fruitService = retrieveFruitsService(apiClient)
+})
+
+describe('FruitService', () => {
+  describe('getFruits', () => {
+    beforeEach(() => {
+      apiClient.get.mockResolvedValue({ data })
+    })
+    it('should retrieve a list', async () => {
+      expect(await fruitService.getFruits()).toContainEqual({
+        isFruit: true,
+        name: 'kiwi',
+        image:
+          'https://raw.githubusercontent.com/cycloidio/fictional-eureka/master/assets/kiwi.jpg',
+        price: '553.00',
+        color: '#4e346b',
+        description:
+          'Kiwifruit (often shortened to kiwi outside Australia and New Zealand), or Chinese gooseberry, is the edible berry of several species of woody vines in the genus Actinidia.',
+        taste: 'Awesome',
+        expires: '2021-04-11T17:33:11.120Z',
+        id: 6,
+      })
+    })
+
+    it('should call the correct url', async () => {
+      await fruitService.getFruits()
+      expect(apiClient.get).toHaveBeenCalledWith('/fruit')
+    })
+
+    it('should retrieve deeply nested fruits', async () => {
+      expect(await fruitService.getFruits()).toContainEqual({
+        isFruit: true,
+        name: 'pineapple',
+        image:
+          'https://raw.githubusercontent.com/cycloidio/fictional-eureka/master/assets/pineapple.jpg',
+        price: '217.00',
+        color: '#42072c',
+        description:
+          'The pineapple (Ananas comosus) is a tropical plant with an edible fruit and the most economically significant plant in the family Bromeliaceae.',
+        taste: 'Fantastic',
+        expires: '2021-06-16T07:50:58.104Z',
+        id: 1,
+      })
+    })
+
+    it("shouldn't retrieve items where isFruit equals false", async () => {
+      expect(await fruitService.getFruits()).not.toContainEqual({
+        isFruit: false,
+        name: 'eggplant',
+        image:
+          'https://raw.githubusercontent.com/cycloidio/fictional-eureka/master/assets/eggplant.jpg',
+        price: '346.00',
+        color: '#2e2f4b',
+        description:
+          'Most commonly purple, the spongy, absorbent fruit is used in several cuisines. Typically used as a vegetable in cooking, it is a berry by botanical definition. As a member of the genus Solanum, it is related to the tomato and potato. Like the tomato, its skin and seeds can be eaten, but, like the potato, it is usually eaten cooked. Eggplant is nutritionally low in macronutrient and micronutrient content, but the capability of the fruit to absorb oils and flavors into its flesh through cooking expands its use in the culinary arts.',
+        taste: 'Rustic',
+        expires: '2021-04-27T19:02:38.958Z',
+        id: '10c78923-d978-45c7-a999-904853c2df49',
+      })
+    })
+
+    it("shouldn't retrieve misleading values ", async () => {
+      expect(await fruitService.getFruits()).not.toContainEqual({
+        isFruit: true,
+      })
+    })
+
+    it('should retrieve added fruits', async () => {
+      expect(await fruitService.getFruits()).toContainEqual({
+        isFruit: true,
+        name: 'pear',
+        image:
+          'https://raw.githubusercontent.com/cycloidio/fictional-eureka/master/assets/kiwi.jpg',
+        price: '553.00',
+        color: '#4e346b',
+        description:
+          'Kiwifruit (often shortened to kiwi outside Australia and New Zealand), or Chinese gooseberry, is the edible berry of several species of woody vines in the genus Actinidia.',
+        taste: 'Awesome',
+        expires: '2021-04-11T17:33:11.120Z',
+        id: 7,
+      })
+    })
+  })
+
+  describe('getFrutById', () => {
+    it('should call the correct url', async () => {
+      const fruit = {
+        isFruit: true,
+        name: 'pear',
+        image:
+          'https://raw.githubusercontent.com/cycloidio/fictional-eureka/master/assets/kiwi.jpg',
+        price: '553.00',
+        color: '#4e346b',
+        description:
+          'Kiwifruit (often shortened to kiwi outside Australia and New Zealand), or Chinese gooseberry, is the edible berry of several species of woody vines in the genus Actinidia.',
+        taste: 'Awesome',
+        expires: '2021-04-11T17:33:11.120Z',
+        id: 7,
+      }
+      apiClient.get.mockResolvedValueOnce({ data: fruit })
+      await fruitService.getFruitById(fruit.id)
+      expect(apiClient.get).toHaveBeenCalledWith(`/fruit/7`)
+    })
+
+    it('should fetch the right data', async () => {
+      const fruit = {
+        isFruit: true,
+        name: 'pear',
+        image:
+          'https://raw.githubusercontent.com/cycloidio/fictional-eureka/master/assets/kiwi.jpg',
+        price: '553.00',
+        color: '#4e346b',
+        description:
+          'Kiwifruit (often shortened to kiwi outside Australia and New Zealand), or Chinese gooseberry, is the edible berry of several species of woody vines in the genus Actinidia.',
+        taste: 'Awesome',
+        expires: '2021-04-11T17:33:11.120Z',
+        id: 7,
+      }
+      apiClient.get.mockResolvedValueOnce({ data: fruit })
+      expect(await fruitService.getFruitById(fruit.id)).toStrictEqual({
+        isFruit: true,
+        name: 'pear',
+        image:
+          'https://raw.githubusercontent.com/cycloidio/fictional-eureka/master/assets/kiwi.jpg',
+        price: '553.00',
+        color: '#4e346b',
+        description:
+          'Kiwifruit (often shortened to kiwi outside Australia and New Zealand), or Chinese gooseberry, is the edible berry of several species of woody vines in the genus Actinidia.',
+        taste: 'Awesome',
+        expires: '2021-04-11T17:33:11.120Z',
+        id: 7,
+      })
+    })
+  })
+
+  describe('deleteFruit', () => {
+    it('should call the right url', async () => {
+      const fruit = {
+        isFruit: true,
+        name: 'pear',
+        image:
+          'https://raw.githubusercontent.com/cycloidio/fictional-eureka/master/assets/kiwi.jpg',
+        price: '553.00',
+        color: '#4e346b',
+        description:
+          'Kiwifruit (often shortened to kiwi outside Australia and New Zealand), or Chinese gooseberry, is the edible berry of several species of woody vines in the genus Actinidia.',
+        taste: 'Awesome',
+        expires: '2021-04-11T17:33:11.120Z',
+        id: 7,
+      }
+
+      apiClient.delete.mockImplementationOnce((id) => id === fruit.id)
+      await fruitService.deleteFruit(fruit.id)
+
+      expect(apiClient.delete).toHaveBeenCalledWith('/fruit/7')
+    })
+  })
+
+  describe('addFruit', () => {
+    it('should call the right url', async () => {
+      const fruit = {
+        isFruit: true,
+        name: 'pear',
+        image:
+          'https://raw.githubusercontent.com/cycloidio/fictional-eureka/master/assets/kiwi.jpg',
+        price: '553.00',
+        color: '#4e346b',
+        description:
+          'Kiwifruit (often shortened to kiwi outside Australia and New Zealand), or Chinese gooseberry, is the edible berry of several species of woody vines in the genus Actinidia.',
+        taste: 'Awesome',
+        expires: '2021-04-11T17:33:11.120Z',
+        id: 7,
+      }
+
+      apiClient.post.mockImplementationOnce((payload) => payload === fruit)
+      await fruitService.addFruit(fruit)
+
+      expect(apiClient.post).toHaveBeenCalledWith('/fruit', fruit)
+    })
+  })
+})
---
+++ tests/unit/data.js
@@ -0,0 +1,314 @@
+export default {
+  what: {
+    is: {
+      here: {
+        isFruit: true,
+        name: 'kiwi',
+        image:
+          'https://raw.githubusercontent.com/cycloidio/fictional-eureka/master/assets/kiwi.jpg',
+        price: '553.00',
+        color: '#4e346b',
+        description:
+          'Kiwifruit (often shortened to kiwi outside Australia and New Zealand), or Chinese gooseberry, is the edible berry of several species of woody vines in the genus Actinidia.',
+        taste: 'Awesome',
+        expires: '2021-04-11T17:33:11.120Z',
+        id: 6,
+      },
+    },
+  },
+  amIaFruit: {
+    name: 'Osborne Hoppe',
+    username: 'Oral_Reichert',
+    email: 'Brandi_OConnell@hotmail.com',
+    address: {
+      street: 'Yesenia Divide',
+      suite: 'Apt. 260',
+      city: 'Billyhaven',
+      zipcode: '19984',
+      geo: {
+        lat: '8.4447',
+        lng: '-28.2535',
+      },
+    },
+    phone: '(835) 685-3505',
+    website: 'leone.com',
+    company: {
+      name: 'Okuneva, Botsford and Deckow',
+      catchPhrase: 'Profound zero administration forecast',
+      bs: 'integrated syndicate infrastructures',
+    },
+  },
+  iDidntThinkSo: {
+    isFruit: true,
+    name: 'pineapple',
+    image:
+      'https://raw.githubusercontent.com/cycloidio/fictional-eureka/master/assets/pineapple.jpg',
+    price: '217.00',
+    color: '#42072c',
+    description:
+      'The pineapple (Ananas comosus) is a tropical plant with an edible fruit and the most economically significant plant in the family Bromeliaceae.',
+    taste: 'Fantastic',
+    expires: '2021-06-16T07:50:58.104Z',
+    id: 1,
+  },
+  meNeither: {
+    isFruit: true,
+    name: 'apple',
+    image:
+      'https://raw.githubusercontent.com/cycloidio/fictional-eureka/master/assets/apple.jpg',
+    price: '15.00',
+    color: '#3a6317',
+    description: 'An apple is an edible fruit produced by an apple tree.',
+    taste: 'Licensed',
+    expires: '2021-03-12T03:10:47.170Z',
+    id: 5,
+  },
+  maybe: {
+    this: {
+      name: 'Mayra Jones',
+      username: 'Ramona_Bosco76',
+      email: 'Hayley.Schumm30@gmail.com',
+      address: {
+        street: 'Gabriella Isle',
+        suite: 'Suite 565',
+        city: 'Hettieton',
+        zipcode: '37844',
+        geo: {
+          lat: '34.7676',
+          lng: '167.1738',
+        },
+      },
+      phone: '741.528.9778 x47645',
+      website: 'maxine.info',
+      company: {
+        name: 'Crona - Stiedemann',
+        catchPhrase: 'Cross-platform homogeneous ability',
+        bs: 'integrated facilitate metrics',
+      },
+    },
+    or: {
+      'this?': {
+        name: 'Fae',
+        username: 'Fae_White57',
+        avatar:
+          'https://s3.amazonaws.com/uifaces/faces/twitter/2fockus/128.jpg',
+        email: 'Fae_White5777@hotmail.com',
+        dob: '1980-02-11T15:17:54.060Z',
+        phone: '642.849.5122 x7541',
+        address: {
+          street: 'Hunter Mountain',
+          suite: 'Apt. 423',
+          city: 'Albertabury',
+          zipcode: '66091-0951',
+          geo: {
+            lat: '81.1611',
+            lng: '146.9360',
+          },
+        },
+        website: 'dorcas.biz',
+        company: {
+          name: 'Hagenes and Sons',
+          catchPhrase: 'Stand-alone real-time time-frame',
+          bs: 'strategic architect users',
+        },
+        does: {
+          it: {
+            ever: {
+              end: {
+                isFruit: true,
+                name: 'banana',
+                image:
+                  'https://raw.githubusercontent.com/cycloidio/fictional-eureka/master/assets/banana.jpg',
+                price: '493.00',
+                color: '#305a59',
+                description:
+                  'A banana is an elongated, edible fruit – botanically a berry – produced by several kinds of large herbaceous flowering plants in the genus Musa.',
+                taste: 'Small',
+                expires: '2022-03-03T22:52:07.858Z',
+                id: 3,
+              },
+            },
+          },
+        },
+      },
+    },
+  },
+  i: {
+    will: {
+      never: {
+        code: {
+          again: {
+            name: 'Elaina Romaguera',
+            username: 'Kellen31',
+            email: 'Joseph_Paucek@gmail.com',
+            address: {
+              streetA: 'Schmeler Branch',
+              streetB: '3248 Marvin Highway',
+              streetC: '3366 Maurine Street Apt. 191',
+              streetD: 'Apt. 808',
+              city: 'Arthurmouth',
+              state: 'Maryland',
+              country: 'Somalia',
+              zipcode: '34323',
+              geo: {
+                lat: '87.1425',
+                lng: '-93.9614',
+              },
+            },
+            phone: '(853) 640-1327',
+            website: 'easton.info',
+            company: {
+              name: 'Schinner LLC',
+              catchPhrase: 'De-engineered bi-directional product',
+              bs: 'killer facilitate ROI',
+            },
+            posts: [
+              {
+                words: 'laborum doloremque nihil',
+                sentence: 'Perferendis sunt soluta aut.',
+                sentences:
+                  'Sed rem aut minus enim dicta nam. Odio perferendis pariatur asperiores est. Nam ducimus aspernatur eum odit. Autem voluptatibus iusto quia modi nihil doloribus distinctio.',
+                paragraph:
+                  'Ratione facere odio maiores pariatur et aut. Est repellat dicta sit ipsam ad. Minus autem suscipit tempora assumenda doloribus dolores ducimus similique nihil.',
+              },
+              {
+                words: 'quia exercitationem autem',
+                sentence: 'Accusamus eos maiores.',
+                sentences:
+                  'Impedit et aut occaecati voluptatem non qui mollitia. Sit non facilis corrupti molestiae ea assumenda quis.',
+                paragraph:
+                  'Iure sit officia. Ut accusantium atque officiis pariatur illum vitae exercitationem non. Quis voluptatem sit. Est et quia est quasi aut doloribus consequatur.',
+              },
+              {
+                words: 'tenetur fuga autem',
+                sentence: 'Enim voluptates deleniti quae itaque quas deserunt.',
+                sentences:
+                  'Libero similique qui. Nobis perferendis enim ipsum eligendi.',
+                paragraph:
+                  'Nostrum laborum et modi omnis. Neque laudantium quae et dolores et consequatur sed sunt. Qui minima laudantium in dolorem similique debitis praesentium sit autem. In non et unde autem fuga sunt eligendi ut et.',
+              },
+            ],
+            accountHistory: [
+              {
+                amount: '572.06',
+                date: '2012-02-01T23:00:00.000Z',
+                business: 'Rolfson LLC',
+                name: 'Personal Loan Account 5691',
+                type: 'payment',
+                account: '22936407',
+              },
+              {
+                amount: '450.04',
+                date: '2012-02-01T23:00:00.000Z',
+                business: 'Moen - Beer',
+                name: 'Investment Account 9188',
+                type: 'invoice',
+                account: '60456165',
+              },
+              {
+                amount: '38.80',
+                date: '2012-02-01T23:00:00.000Z',
+                business: 'Zieme, Padberg and Harris',
+                name: 'Home Loan Account 1117',
+                type: 'deposit',
+                account: '01004246',
+              },
+            ],
+          },
+        },
+      },
+      or: {
+        maybe: {
+          i: {
+            will: {
+              isFruit: false,
+              name: 'eggplant',
+              image:
+                'https://raw.githubusercontent.com/cycloidio/fictional-eureka/master/assets/eggplant.jpg',
+              price: '346.00',
+              color: '#2e2f4b',
+              description:
+                'Most commonly purple, the spongy, absorbent fruit is used in several cuisines. Typically used as a vegetable in cooking, it is a berry by botanical definition. As a member of the genus Solanum, it is related to the tomato and potato. Like the tomato, its skin and seeds can be eaten, but, like the potato, it is usually eaten cooked. Eggplant is nutritionally low in macronutrient and micronutrient content, but the capability of the fruit to absorb oils and flavors into its flesh through cooking expands its use in the culinary arts.',
+              taste: 'Rustic',
+              expires: '2021-04-27T19:02:38.958Z',
+              id: '10c78923-d978-45c7-a999-904853c2df49',
+            },
+          },
+        },
+        who: {
+          knows: {
+            isFruit: true,
+            name: 'tomato',
+            image:
+              'https://raw.githubusercontent.com/cycloidio/fictional-eureka/master/assets/tomato.jpg',
+            price: '148.00',
+            color: '#2f4756',
+            description:
+              'The tomato is the edible, often red, berry of the plant Solanum lycopersicum, commonly known as a tomato plant. The species originated in western South America and Central America.',
+            taste: 'Licensed',
+            expires: '2021-09-28T02:11:36.874Z',
+            id: 4,
+          },
+        },
+        fruit: {
+          isFruit: true,
+          name: 'orange',
+          image:
+            'https://raw.githubusercontent.com/cycloidio/fictional-eureka/master/assets/orange.jpg',
+          price: '736.00',
+          color: '#343025',
+          description:
+            'The orange is the fruit of various citrus species in the family Rutaceae (see list of plants known as orange); it primarily refers to Citrus × sinensis, which is also called sweet orange, to distinguish it from the related Citrus × aurantium, referred to as bitter orange. The sweet orange reproduces asexually (apomixis through nucellar embryony); varieties of sweet orange arise through mutations.',
+          taste: 'Intelligent',
+          expires: '2021-03-30T22:40:30.391Z',
+          id: 2,
+        },
+      },
+      fruit: false,
+      lol: {
+        name: 'Joan',
+        username: 'Joan.Gleichner',
+        avatar:
+          'https://s3.amazonaws.com/uifaces/faces/twitter/apriendeau/128.jpg',
+        email: 'Joan.Gleichner_Mayer@hotmail.com',
+        dob: '1946-11-14T11:31:50.732Z',
+        phone: '372-734-2508 x847',
+        address: {
+          street: 'Boehm Street',
+          suite: 'Suite 562',
+          city: 'North Camylle',
+          zipcode: '80853-8720',
+          geo: {
+            lat: '36.1179',
+            lng: '19.3748',
+          },
+        },
+        website: 'mohammed.net',
+        company: {
+          name: 'Runolfsdottir, Weber and Orn',
+          catchPhrase: 'Robust impactful workforce',
+          bs: 'rich unleash convergence',
+        },
+      },
+    },
+  },
+  more: [
+    {
+      isFruit: true,
+      name: 'pear',
+      image:
+        'https://raw.githubusercontent.com/cycloidio/fictional-eureka/master/assets/kiwi.jpg',
+      price: '553.00',
+      color: '#4e346b',
+      description:
+        'Kiwifruit (often shortened to kiwi outside Australia and New Zealand), or Chinese gooseberry, is the edible berry of several species of woody vines in the genus Actinidia.',
+      taste: 'Awesome',
+      expires: '2021-04-11T17:33:11.120Z',
+      id: 7,
+    },
+  ],
+  here: {
+    isFruit: true,
+  },
+  fruitCount: 6,
+}
