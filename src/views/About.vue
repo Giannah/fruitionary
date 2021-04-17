26b6eb913359432d4150e5a75a7a7931842101f6
Claudine LAFINE <claudine.lafine@gmail.com>
2021-04-17 21:10:25 +0200

    some fun

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,23 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div>
+    <h1>Are you a fruit lover?</h1>
+    <img
+      src="https://www.unlockfood.ca/EatRightOntario/media/Website-images-resized/How-to-store-fruit-to-keep-it-fresh-resized.jpg"
+      alt="fruits"
+    />
+    <div class="about-text">
+      <p>
+        This fruitionary is your playground, feel free to play around! Add or
+        remove fruits as you want or just go to a fruit page to know more about
+        it.
+      </p>
+      <p>Have fun !</p>
+    </div>
+    <el-divider><i class="el-icon-watermelon"></i></el-divider>
   </div>
 </template>
+<style lang="scss" scoped>
+.about-text {
+  font-size: 1.2rem;
+}
+</style>
