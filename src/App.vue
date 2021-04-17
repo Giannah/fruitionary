272a5392020798db3326da4bb5c5c51316fc13d6
Claudine LAFINE <claudine.lafine@gmail.com>
2021-04-17 20:09:21 +0200

    App

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,20 @@
 <template>
   <div id="nav">
-    <router-link to="/">Home</router-link> |
+    <router-link to="/">Home</router-link>
+    <el-divider direction="vertical" class="divider"></el-divider>
     <router-link to="/about">About</router-link>
   </div>
-  <router-view />
+  <router-view v-slot="{ Component }">
+    <transition name="fade">
+      <component :is="Component" />
+    </transition>
+  </router-view>
 </template>
 
 <style lang="scss">
+body {
+  margin: 1.2rem;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -16,15 +24,34 @@
 }
 
 #nav {
-  padding: 30px;
+  padding: 2rem;
 
   a {
+    font-size: 2rem;
     font-weight: bold;
     color: #2c3e50;
+    text-decoration: none;
 
     &.router-link-exact-active {
-      color: #42b983;
+      color: #c1aa28;
     }
   }
+  .divider {
+    margin-right: 1rem;
+    margin-left: 1rem;
+  }
+}
+
+.fade-enter-active {
+  transition: all 0.3s ease;
+}
+.fade-leave-active {
+  transition: all 0.8s cubic-bezier(1, 0.5, 0.8, 1);
+}
+
+.fade-enter,
+.fade-leave-to {
+  transform: translateX(10px);
+  opacity: 0;
 }
 </style>
